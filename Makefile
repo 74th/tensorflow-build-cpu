28f2ccd50b73e36adc61be62fd5e01e96ddaa27b
74th <site@j74th.com>
2018-07-01 13:17:26 +0000

    first

---
+++ Makefile
@@ -0,0 +1,10 @@
+build-container:
+	docker build -t tensorflow-cpu-py3.7.0 --build-arg PYTHON_VER="3.7.0" .
+build:
+	docker run -it --rm \
+		-v `pwd`/scripts:/root/scripts \
+		-v `pwd`/tensorflow:/root/tensorflow \
+		tensorflow-cpu-py3.7.0 \
+		/root/scripts/build.sh
+clean:
+	rm -rf tensorflow/* tensorflow/.*
---
+++ scripts/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# -C no overwrite, -e stop at error, -u stop at undefined variable, -x print commands
+set -Ceux
+TENSORFLOW_VER=r1.8
+PYTHON_VER=3.7.0
+
+export PATH=~/.pyenv/bin:${PATH}
+#pyenv install ${PYTHON_VER}
+
+#mkdir -p tensorflow
+#git clone https://github.com/tensorflow/tensorflow.git -b ${TENSORFLOW_VER} tensorflow/
+cd tensorflow/
+export PATH=/root/.pyenv/shims:$PATH
+
+# see https://github.com/tensorflow/tensorflow/blob/master/configure.py
+export PYTHON_BIN_PATH=$(which python)
+export PYTHON_LIB_PATH="$($PYTHON_BIN_PATH -c 'import site; print(site.getsitepackages()[0])')"
+export TF_NEED_S3=1
+export TF_NEED_GCP=1
+export TF_NEED_HDFS=1
+export TF_NEED_JEMALLOC=1
+export TF_NEED_CUDA=0
+export TF_NEED_OPENCL=0
+export TF_NEED_OPENCL_SYCL=0
+export TF_DOWNLOAD_CLANG=0
+export TF_NEED_KAFKA=0
+export TF_ENABLE_XLA=0
+export TF_NEED_VERBS=0
+export TF_NEED_GDR=0
+export TF_NEED_MKL=1
+export TF_DOWNLOAD_MKL=1
+export TF_NEED_MPI=0
+export TF_SET_ANDROID_WORKSPACE=0
+
+export GCC_HOST_COMPILER_PATH=$(which gcc)
+export CC_OPT_FLAGS="-march=native"
+#bazel clean
+./configure
+bazel build -c opt --copt=-mfpmath=both --copt=-mavx --copt=-msse4.1 --copt=-msse4.2 //tensorflow/tools/pip_package:build_pip_package
